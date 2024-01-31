839a0065abd78024cef87e1cada66464b61c06a7
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-01-31 22:36:14 +0000

    Using Link component and moving out into separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,12 @@
 import Image from "next/image";
+import Link from 'next/link';
+import NavLink from '@/src/ui/nav-links'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
-       <div className="grid text-center bg-inherit lg:w-full lg:mb-0 lg:grid-cols-5 lg:text-left">
-        <a
-          href="/"
-          className="group rounded-lg border border-transparent px-6 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Home{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Navigate through our site from here.
-          </p>
-        </a>
-
-        <a
-          href="/about"
-          className="group rounded-lg border border-transparent px-6 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            About{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about McNamee Coach Hire.
-          </p>
-        </a>
-
-        <a
-          href="/contact"
-          className="group rounded-lg border border-transparent px-6 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Contact Us{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find our contact details here or use our Contact form!
-          </p>
-        </a>
-
-        <a
-          href="/gallery"
-          className="group rounded-lg border border-transparent px-6 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Gallery{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            See pictures of our current fleet.
-          </p>
-        </a>
-        <a
-          href="/routes"
-          className="group rounded-lg border border-transparent px-6 py-6 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Routes{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            See information about the current routes we operate.
-          </p>
-        </a>
-      </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+       <NavLink />
+      {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -122,7 +40,7 @@ export default function Home() {
           height={37}
           priority
         />
-      </div>
+      </div> */}
 
      
     </main>
