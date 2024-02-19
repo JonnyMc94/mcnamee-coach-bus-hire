796164ae2c48f8f4254bc5a9a1a6380e50c7780a
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-02-19 22:54:36 +0000

    Finalising carousel choice and overlaying image with contact call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import ImageCarousel from "@/src/ui/image-carousel"
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <SmallContactBar />
       <NavLink />
-      <ImageCarousel />
+      <ImageCarousel />      
       <Footer />
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
--- src/ui/image-carousel.tsx
+++ src/ui/image-carousel.tsx
@@ -1,32 +1,78 @@
 "use client";
-import Image from 'next/image';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from 'react-slick';
-
+import Image from "next/image";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function ImageCarousel() {
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    cssEase: "linear",
-  };
+  const options = { loop: true, playOnInit: true, delay: 20000 };
+  const [emblaRef] = useEmblaCarousel(options, [Autoplay(options)]);
 
   return (
-    <div className="h-70vh sm:h-84vh xl:h-80 2xl:h-[60vh]">
-      <Slider {...settings}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <Image alt="" src="/coach1.png" height={400} width={300}/>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <Image alt="" src="/coach2.png" height={400} width={300}/>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <Image alt="" src="/coach3.png" height={400} width={300}/>
-      </Slider>
+    <div className="overflow-hidden" ref={emblaRef}>
+      <div className="flex">
+        <div className="relative h-[50vh] flex-[0_0_100%]">
+          {" "}
+          <div className="absolute z-10 w-[50vh] h-[34vh] left-1/3 top-28 bg-inherit">
+            <div className="flex flex-row items-center justify-start">
+              <Link
+                className="pl-4 pr-3 pt-2 pb-2 mr-2 bg-[#130F49] text-gray-50 font-light rounded-xl text-2xl flex justify-between"
+                href="/contact"
+              >
+                {" "}
+                Contact Us
+                <FaArrowRight className="text-black ml-2 m-1 text-2xl" />
+              </Link>
+            </div>
+            <h2 className="text-6xl text-white font-extrabold mt-8">Get in contact today!</h2>
+            <h2 className="text-2xl text-white font-bold mt-6">Find a bus or coach to suit your need!</h2>
+          </div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <Image alt="" fill src="/coach1.png" className="object-fill z-0" />
+        </div>
+
+        <div className="relative h-[50vh] flex-[0_0_100%]">
+          {" "}
+          <div className="absolute z-10 w-[50vh] h-[34vh] left-1/3 top-28 bg-inherit">
+            <div className="flex flex-row items-center justify-start">
+              <Link
+                className="pl-4 pr-3 pt-2 pb-2 mr-2 bg-[#130F49] text-gray-50 font-light rounded-xl text-2xl flex justify-between"
+                href="/contact"
+              >
+                {" "}
+                Contact Us
+                <FaArrowRight className="text-black ml-2 m-1 text-2xl" />
+              </Link>
+            </div>
+            <h2 className="text-6xl text-white font-extrabold mt-8">Get in contact today!</h2>
+            <h2 className="text-2xl text-white font-bold mt-6">Find a bus or coach to suit your need!</h2>
+          </div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <Image alt="" fill src="/coach2.png" className="object-fill" />
+        </div>
+
+        <div className="relative h-[50vh] flex-[0_0_100%]">
+          {" "}
+          <div className="absolute z-10 w-[50vh] h-[34vh] left-1/3 top-28 bg-inherit">
+            <div className="flex flex-row items-center justify-start">
+              <Link
+                className="pl-4 pr-3 pt-2 pb-2 mr-2 bg-[#130F49] text-gray-50 font-light rounded-xl text-2xl flex justify-between"
+                href="/contact"
+              >
+                {" "}
+                Contact Us
+                <FaArrowRight className="text-black ml-2 m-1 text-2xl" />
+              </Link>
+            </div>
+            <h2 className="text-6xl text-white font-extrabold mt-8">Get in contact today!</h2>
+            <h2 className="text-2xl text-white font-bold mt-6">Find a bus or coach to suit your need!</h2>
+          </div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <Image alt="" fill src="/coach3.png" className="object-fill" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
