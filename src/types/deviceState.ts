3770858a0469a22d05828bb91a7ef718429e2db4
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-03-05 11:43:02 +0000

    Final updates to whatsapp message

--- src/types/deviceState.ts
+++ src/types/deviceState.ts
@@ -1,4 +1,4 @@
-const userAgent: string = navigator.userAgent || navigator.vendor || (window as any).opera || undefined;
+const userAgent: string = typeof navigator !== 'undefined' ? navigator.userAgent || navigator.vendor || (window as any).opera || undefined : '';
 
 export const isMobileDevice = (): boolean => {
   const regexs = [/(Android)(.+)(Mobile)/i, /BlackBerry/i, /iPhone|iPod/i, /Opera Mini/i, /IEMobile/i];
