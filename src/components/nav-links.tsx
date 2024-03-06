1f6277a7552ee73dc189284f90701587e6f928e4
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-03-06 22:12:02 +0000

    UI of Hamburger menu

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -22,6 +23,12 @@ export default function NavLink() {
   ];
 
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  //Handles the opening and closing of our nav
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className="flex justify-between items-center w-[100%] h-[8vh] bg-blue-400">
@@ -45,7 +52,7 @@ export default function NavLink() {
           })}
         </ul>
       </div>
-      <div className="flex flex-row items-center justify-start mr-2 text-black">
+      <div className="md:flex md:flex-row md:items-center md:justify-start md:mr-2 md:text-black hidden">
         <Link
           className="pl-4 pr-3 pt-2 pb-2 mr-2 bg-blue-700 text-gray-50 font-light rounded-xl text-base flex flex-row items-center"
           href="/contact"
@@ -53,6 +60,44 @@ export default function NavLink() {
           {" "}
           Contact Us <FaArrowRight className="text-white ml-2" />
         </Link>
+      </div>
+      <div className="flex flex-col md:flex-row md:items-center">
+        <button
+          onClick={handleClick}
+          className="flex flex-col justify-center items-center mr-10 md:hidden"
+        >
+          <span
+            className={`bg-black block transition-all duration-300 ease-out 
+                    h-0.5 w-6 rounded-sm ${
+                      isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+                    }`}
+          ></span>
+          <span
+            className={`bg-black block transition-all duration-300 ease-out 
+                    h-0.5 w-6 rounded-sm my-0.5 ${
+                      isOpen ? "opacity-0" : "opacity-100"
+                    }`}
+          ></span>
+          <span
+            className={`bg-black block transition-all duration-300 ease-out 
+                    h-0.5 w-6 rounded-sm ${
+                      isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+                    }`}
+          ></span>
+        </button>
+        {isOpen && (
+          <div className="md:absolute top-full left-0 md:flex justify-between items-center w-full bg-blue-400 md:bg-transparent">
+            <ul className="flex flex-col md:flex-row items-center gap-4 md:gap-8">
+              {links.map((data, id) => (
+                <li key={id}>
+                  <a className="hover:text-gray-500 text-xl" href={data.href}>
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
