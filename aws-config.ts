42e6af5954ebffd587f260b8e778595dd88b1523
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-06-14 12:48:05 -0700

    Updating config

--- aws-config.ts
+++ aws-config.ts
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION
 });
 
 export default AWS;
