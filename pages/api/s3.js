65d978698916df208ef0c152639c724211f97661
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-06-14 19:59:00 -0700

    Updating logging

--- pages/api/s3.js
+++ pages/api/s3.js
@@ -53,6 +53,6 @@ export default async function handler(req, res) {
       res.status(200).json({ data });
     } catch (error) {
       console.log(error.message);
-      res.status(500).json({ error: 'Error generating signed URLs and getting metadata' });
+      res.status(500).json({ error: error.message });
     }
   }
