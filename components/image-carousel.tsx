cebc49583526d95229dd44f23b6ed98f75e51186
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-06-16 21:11:31 -0700

    Reworking api call

--- components/image-carousel.tsx
+++ components/image-carousel.tsx
@@ -32,36 +32,31 @@ export default function ImageCarousel() {
   return (
     <div className="relative overflow-hidden">
       <div className="flex">
-        
-          <div className="relative flex-shrink-0 h-[89vh] w-full">
-            <Image
-              alt={imageData?.metadata.Metadata.alt ?? "Two coaches at nighttime"}
-              aria-label={imageData?.metadata.Metadata['aria-label'] ?? "Two coaches."}
-              fill
-              src={imageData?.url ?? ""}
-              className="absolute inset-0 object-cover w-full h-full"
-            />
-            <div className="overlay absolute inset-0 bg-black opacity-30"></div>
-            <div className="absolute inset-0 flex flex-col items-center mt-36 text-white">
-              <div className="flex flex-row items-center justify-start mb-4">
-                <Link
-                  className="pl-4 pr-3 pt-2 pb-2 mr-2 bg-blue-700 md:hidden text-gray-50 font-light rounded-xl text-base flex"
-                  href="/contact"
-                >
+        <div className="relative flex-shrink-0 h-[89vh] w-full">
+          <img
+            alt={imageData?.metadata?.Metadata?.alt ?? "Two coaches at nighttime"}
+            // aria-label={imageData?.metadata?.Metadata?['aria-label'] ?? "Two coaches."}
+            src={imageData?.url ?? ""}
+            className="absolute inset-0 object-cover w-full h-full"
+          />
+          <div className="overlay absolute inset-0 bg-black opacity-30"></div>
+          <div className="absolute inset-0 flex flex-col items-center mt-36 text-white">
+            <div className="flex flex-row items-center justify-start mb-4">
+              <Link passHref href="/contact">
+                <a className="pl-4 pr-3 pt-2 pb-2 mr-2 bg-blue-700 md:hidden text-gray-50 font-light rounded-xl text-base flex">
                   Contact Us {isContactPage ? <FaArrowDown className="text-white ml-2 mt-1" /> : <FaArrowRight className="text-white ml-2 mt-1" />}
-                </Link>
-              </div>
-              <h2 className=" block 4sm:text-xl lg:text-6xl md:text-4xl sm:text-4xl xsm:text-2xl xxsm:text-2xl xxxsm:text-xl text-white font-extrabold text-center">
-                Get in contact today!
-              </h2>
-              <h2 className="block 4sm:text-base lg:text-2xl md:text-xl sm:text-base xsm:text-sm xxsm:text-sm xxxsm:text-base text-white font-bold mt-6 text-center">
-                Find a bus or coach to suit your needs!
-              </h2>
+                </a>
+              </Link>
             </div>
+            <h2 className=" block 4sm:text-xl lg:text-6xl md:text-4xl sm:text-4xl xsm:text-2xl xxsm:text-2xl xxxsm:text-xl text-white font-extrabold text-center">
+              Get in contact today!
+            </h2>
+            <h2 className="block 4sm:text-base lg:text-2xl md:text-xl sm:text-base xsm:text-sm xxsm:text-sm xxxsm:text-base text-white font-bold mt-6 text-center">
+              Find a bus or coach to suit your needs!
+            </h2>
           </div>
-        
+        </div>
       </div>
     </div>
   );
 }
-
--- pages/api/s3.js
+++ pages/api/s3.js
@@ -1,4 +1,4 @@
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
 
 AWS.config.update({
   accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
@@ -6,70 +6,35 @@ AWS.config.update({
   region: process.env.NEXT_PUBLIC_REGION,
 });
 
-AWS.config.logger = console; 
+const s3 = new AWS.S3();
 
 export default async function handler(req, res) {
-    console.log('Handler function called');
-  const s3 = new AWS.S3({
-    endpoint: "https://s3.ca-central-1.amazonaws.com",
-  });
-  const { keys } = req.body || []; // Extract keys from request body, default to empty array if no keys are provided
-  console.log('Keys:', keys);
+  const { keys = [] } = req.body; // Extract keys from request body, default to empty array if no keys are provided
+
   try {
-    let data;
-    if (keys.length > 0) {
-      // If keys are provided, generate signed URLs and get metadata for the corresponding images
-      data = await Promise.all(
-        keys.map(async (key) => {
-          const url = s3.getSignedUrl("getObject", {
-            Bucket: "mcnamee-coach-hire-gallery",
-            Key: key,
-            Expires: 60 * 60, // URL will be valid for 1 hour
-          });
-
-          console.log('URL:', url);
+    const data = await Promise.all(
+      keys.map(async (key) => {
+        try {
+          const url = `https://mcnamee-coach-hire-gallery.s3.ca-central-1.amazonaws.com/${key}`;
 
           const metadata = await s3
             .headObject({
-              Bucket: "mcnamee-coach-hire-gallery",
+              Bucket: 'mcnamee-coach-hire-gallery',
               Key: key,
             })
             .promise();
 
           return { url, metadata };
-        })
-      );
-    } else {
-      // If no keys are provided, generate signed URLs and get metadata for all images
-      const { Contents: keys } = await s3
-        .listObjectsV2({
-          Bucket: "mcnamee-coach-hire-gallery",
-        })
-        .promise();
-
-      data = await Promise.all(
-        keys.map(async ({ Key: key }) => {
-          const url = s3.getSignedUrl("getObject", {
-            Bucket: "mcnamee-coach-hire-gallery",
-            Key: key,
-            Expires: 60 * 60, // URL will be valid for 1 hour
-          });
-
-          const metadata = await s3
-            .headObject({
-              Bucket: "mcnamee-coach-hire-gallery",
-              Key: key,
-            })
-            .promise();
-
-          return { url, metadata };
-        })
-      );
-    }
+        } catch (error) {
+          console.error(`Error processing key ${key}:`, error);
+          return null;
+        }
+      })
+    );
 
     res.status(200).json({ data });
   } catch (error) {
-    console.log(error.message);
+    console.error('Error in handler:', error);
     res.status(500).json({ error: error.message });
   }
 }
