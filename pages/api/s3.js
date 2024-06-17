43916a54d567468cddac0690382aa6ce9a82db4f
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-06-16 20:22:31 -0700

    Adding mroe logging for debugging purposes

--- pages/api/s3.js
+++ pages/api/s3.js
@@ -9,11 +9,12 @@ AWS.config.update({
 AWS.config.logger = console; 
 
 export default async function handler(req, res) {
+    console.log('Handler function called');
   const s3 = new AWS.S3({
     endpoint: "https://s3.ca-central-1.amazonaws.com",
   });
   const { keys } = req.body || []; // Extract keys from request body, default to empty array if no keys are provided
-
+  console.log('Keys:', keys);
   try {
     let data;
     if (keys.length > 0) {
@@ -25,6 +26,8 @@ export default async function handler(req, res) {
             Key: key,
             Expires: 60 * 60, // URL will be valid for 1 hour
           });
+
+          console.log('URL:', url);
 
           const metadata = await s3
             .headObject({
