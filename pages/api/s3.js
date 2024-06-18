db2b3342db556135e3b9597958efcdd0dc94696e
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-06-17 19:50:03 -0700

    More troubleshooting

--- pages/api/s3.js
+++ pages/api/s3.js
@@ -6,6 +6,10 @@ AWS.config.update({
   region: process.env.NEXT_PUBLIC_REGION,
 });
 
+console.log(process.env.NEXT_PUBLIC_ACCESS_KEY_ID);
+console.log(process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY);
+console.log(process.env.NEXT_PUBLIC_REGION);
+
 const s3 = new AWS.S3();
 
 export default async function handler(req, res) {
@@ -23,7 +27,7 @@ export default async function handler(req, res) {
           const url = `https://mcnamee-coach-hire-gallery.s3.ca-central-1.amazonaws.com/${key}`;
 
           const metadata = await s3
-            .headObject({
+            .getObject({
               Bucket: 'mcnamee-coach-hire-gallery',
               Key: key,
             })
@@ -40,6 +44,6 @@ export default async function handler(req, res) {
     res.status(200).json({ data });
   } catch (error) {
     console.error('Error in handler:', error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.toString() });
   }
 }
