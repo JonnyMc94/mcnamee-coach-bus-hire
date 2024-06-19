6384461cbb82383eae2038c8fd537db36f19eff6
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-06-19 15:29:14 -0700

    Updating gallery to pull from s3 bucket

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,118 +1,81 @@
 import Footer from "@/components/copyright-footer";
 import GalleryLayout from "./layout";
-import Head from 'next/head';
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { ImageData } from "../../types/types";
 
 
-const data = [
-  {
-    id: 1,
-    src: "/schoolbus1.JPG",
-    alt: "Image of a white school bus",
-  },
-  {
-    id: 2,
-    src: "/2coach2.JPG",
-    alt: "Image of two coaches",
-  },
-  {
-    id: 3,
-    src: "/2coach3.JPG",
-    alt: "Image of two coaches",
-  },
-  {
-    id: 4,
-    src: "/2coach4.JPG",
-    alt: "Image of two coaches",
-  },
-  {
-    id: 5,
-    src: "/2coach5.JPG",
-    alt: "Image of two coaches",
-  },
-  {
-    id: 6,
-    src: "/2coach2.JPG",
-    alt: "Image of two coaches",
-  },
-  {
-    id: 2,
-    src: "/2coaches.JPG",
-    alt: "Image of two coaches",
-  },
-  {
-    id: 7,
-    src: "/2coachnightime.JPG",
-    alt: "Image of two coaches at nightime",
-  },
-  {
-    id: 8,
-    src: "/bluecoach1.JPG",
-    alt: "Image of a blue coach",
-  },
-  {
-    id: 12,
-    src: "/fleetlineup1.JPG",
-    alt: "Lineup of the fleet",
-  },
-  {
-    id: 13,
-    src: "/greycoach.png",
-    alt: "Image of a grey coach",
-  },
-  {
-    id: 14,
-    src: "/lineup.jpg",
-    alt: "Lineup of the fleet",
-  },
-  {
-    id: 15,
-    src: "/schoolbus1.JPG",
-    alt: "Image of a schoolbus",
-  },
-  {
-    id: 16,
-    src: "/whitecoach1.JPG",
-    alt: "Image of a white coach",
-  },
-  {
-    id: 17,
-    src: "/whitecoach2.JPG",
-    alt: "Image of a white coach",
-  },
-  {
-    id: 18,
-    src: "/whitecoach3.JPG",
-    alt: "Image of a white coach",
-  },
-  {
-    id: 19,
-    src: "/whitecoach4.JPG",
-    alt: "Image of a white coach",
-  },
-];
+const [imageData, setImageData] = useState<ImageData[] | []>([]);
+
+useEffect(() => {
+  fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/s3`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      keys: [
+        "schoolbus1.JPG",
+        "2coach2.JPG",
+        "2coach3.JPG",
+        "2coach4.JPG",
+        "2coach5.JPG",
+        "2coach2.JPG",
+        "2coaches.JPG",
+        "2coachnightime.JPG",
+        "bluecoach1.JPG",
+        "fleetlineup1.JPG",
+        "greycoach.png",
+        "lineup.jpg",
+        "schoolbus1.JPG",
+        "whitecoach1.JPG",
+        "whitecoach2.JPG",
+        "whitecoach3.JPG",
+        "whitecoach4.JPG"
+      ],
+    }),
+  })
+    .then((response) => {
+      console.log("Fetch response:", response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Setting image data:", data);
+      setImageData(data.map((item: ImageData) => ({
+        url: item.url,
+        metadata: item.metadata,
+      })));
+    })
+    .catch((error) => {
+      console.error("Error in fetch call:", error);
+    });
+}, []);
 
 export default function GalleryPage() {
   return (
     <GalleryLayout className="flex-grow items-center justify-center md:h-svh mb-10 p-6">
-            <Head>
+      <Head>
         <title>Gallery Page</title>
-        <meta name="description" content="You can see images of our fleet here" />
+        <meta
+          name="description"
+          content="You can see images of our fleet here"
+        />
       </Head>
       <div className="bg-white min-h-75svh w-full p-8 sm:p-10 lg:p-12">
         <div className="mx-auto max-w-screen-4xl md:px-8">
           <div className="grid grid-cols-3 gap-4 md:gap-6 xl:gap-8">
             {/* <!-- image - start --> */}
-            {data.map((img) => {
+            {imageData.map((img) => {
               return (
                 <a
                   href="#"
                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
-                  key={img.id}
+                  key={img.url}
                 >
                   <img
-                    src={img.src}
+                    src={img.url}
                     loading="lazy"
-                    alt={img.alt}
+                    alt={img.metadata.alt}
                     className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                   />
 
